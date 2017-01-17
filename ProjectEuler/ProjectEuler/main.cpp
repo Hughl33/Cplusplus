13f9f644156b0a369ced4899a1d7db6c1e24eda7
Hughl33 <hughl33@gmail.com>
2017-01-17 22:37:36 +1100

    Added solution for problems 8 - 10

--- ProjectEuler/ProjectEuler/main.cpp
+++ ProjectEuler/ProjectEuler/main.cpp
@@ -143,17 +143,67 @@ std::string problem_seven() {
 }
 
 std::string problem_eight() {
-    int length = 4;
+    int length = 13;
     std::string value = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+    
     long result = 0;
-    for (int i = length - 1; i < value.size(); i++) {
+    while (true) {
         long product = 1;
-        for (int j = i; j >= 0; j--) {
-            char temp = value[j];
+        for (int i = 0; i < length; i++) {
+            char temp = value[i];
             product *= atol(&temp);
         }
         if (result < product) result = product;
+        value.erase(value.begin());
+        if (value.size() < length) break;
     }
+    
+    return std::to_string(result);
+}
+
+std::string problem_nine() {
+    int a = 0;
+    int b = 1;
+    int c = 999;
+    
+    for (int i = 0; i < c; i++) {
+        c--;
+        b++;
+        for (int j = 0; j < b; j++) {
+            c--;
+            a++;
+            if (pow(a, 2) + pow(b, 2) == pow(c, 2)) {
+                return std::to_string(a*b*c);
+            }
+        }
+        c += a - 1;
+        a = 1;
+    }
+    
+    return "Error: No Result";
+}
+
+std::string problem_ten() {
+    long result = 5;
+    long limit = 2000000;
+    
+    for (int i = 2; i < limit; i++) {
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) break;
+            else if (j + 1 > sqrt(i)) {
+                result += i;
+            }
+            
+        }
+    }
+    return std::to_string(result);
+}
+
+std::string problem_eleven() {
+    int result = 0;
+    
+    
+    
     return std::to_string(result);
 }
 
@@ -163,6 +213,6 @@ std::string problem_eight() {
 //}
 
 int main(int argc, const char * argv[]) {
-    std::cout << problem_eight() << std::endl;
+    std::cout << problem_eleven() << std::endl;
     return 0;
 }
